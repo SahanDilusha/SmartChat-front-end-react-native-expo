73828fe910967f9cfaede7b5b5bb2b7939b6d656
SahanDilusha <sahancodelygroup@gmail.com>
2024-09-27 12:40:01 +0530

    "Added TextInput component and updated styles in App.js"

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import { Image, StyleSheet, View,Text } from "react-native";
+import { Image, StyleSheet, View, Text, TextInput } from "react-native";
 import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
 import { useEffect } from "react";
+
 
 SplashScreen.preventAutoHideAsync();
 
@@ -27,13 +28,23 @@ export default function App() {
 
   return (
     <View style={stylesheet.view1}>
-      <Image source={imagePath}/>
+      <Image source={imagePath} />
 
-      <Text>Create Account</Text>
+      <Text style={stylesheet.text1}>Create Account</Text>
 
-      <Text>Hello! Welcome to Smart Chat</Text>
+      <Text style={stylesheet.text2}>Hello! Welcome to Smart Chat</Text>
 
-      
+      <Text style={stylesheet.text3}>Mobile</Text>
+      <TextInput style={stylesheet.input1} />
+
+      <Text style={stylesheet.text3}>First Name</Text>
+      <TextInput style={stylesheet.input1} />
+
+      <Text style={stylesheet.text3}>Last Name</Text>
+      <TextInput style={stylesheet.input1} />
+
+      <Text style={stylesheet.text3}>Password</Text>
+      <TextInput style={stylesheet.input1} />
 
     </View>
   );
@@ -44,5 +55,28 @@ const stylesheet = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
-  }
+    paddingHorizontal: 10,
+    rowGap: 10,
+  },
+  input1: {
+    width: "100%",
+    height: 50,
+    borderWidth: 2,
+    borderRadius: 15,
+    borderStyle: "solid",
+    paddingStart: 10,
+    fontSize:18,
+  },
+  text1:{
+    fontSize:32,
+    fontFamily:"Montserrat-Bold",
+  },
+  text2:{
+    fontSize:20,
+    fontFamily:"Montserrat-Regular",
+  },
+  text3:{
+    fontSize:16,
+    fontFamily:"Montserrat-Bold",
+  },
 });
