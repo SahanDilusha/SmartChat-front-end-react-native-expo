499fef3e4a1d277305f390f73cb5f762fee771eb
SahanDilusha <sahancodelygroup@gmail.com>
2024-09-27 12:09:04 +0530

    "Added Expo SplashScreen and font loading with useFonts hook"

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
 import { StyleSheet, View } from "react-native";
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return(
+
+  const [loaded, error] = useFonts({
+
+    "Montserrat-Bold": "./assets/fonts/Montserrat-Bold.ttf",
+    "Montserrat-Light": "./assets/fonts/Montserrat-Light.ttf",
+    "Montserrat-Regular": "assets/fonts/Montserrat-Regular.ttf",
+
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
     <View style={stylesheet.view1}>
-      
+
     </View>
   );
 }
 
 const stylesheet = StyleSheet.create({
-  view1:{
-    flex:1,
-    backgroundColor:"#fff",
-    justifyContent:"center",
-    alignItems:"center",
+  view1: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   }
 });
