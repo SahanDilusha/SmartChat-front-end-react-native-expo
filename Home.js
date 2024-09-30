5ec223be572b1fcc38dae4a04eb379e601bb28b2
SahanDilusha <sahancodelygroup@gmail.com>
2024-09-30 11:06:38 +0530

    "Added imports, fonts, and styles to Home.js, and modified the component to use LinearGradient and ScrollView."

--- Home.js
+++ Home.js
@@ -1,10 +1,67 @@
 import { registerRootComponent } from "expo";
-import { View } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { useEffect, useState } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 function Home() {
-    return (<View>
 
-    </View>);
+    const [loaded, error] = useFonts({
+        "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+        "Montserrat-Light": require("./assets/fonts/Montserrat-Light.ttf"),
+        "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+    });
+
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null;
+    }
+
+    return (
+        <LinearGradient colors={['#b8d2fc', '#ffffff']} style={stylesheet.view1}>
+
+            <View style={stylesheet.view2}>
+                <View style={stylesheet.view3}>
+
+                </View>
+            </View>
+            <ScrollView style={stylesheet.scrollView1}>
+
+
+            </ScrollView>
+        </LinearGradient>
+    );
 }
 
 registerRootComponent(Home);
+
+const stylesheet = StyleSheet.create({
+    view1: {
+        flex: 1,
+        paddingVertical:50,
+        paddingHorizontal:25,
+    },
+
+    view2: {
+
+    },
+
+    view3: {
+        width: 80,
+        height: 80,
+        backgroundColor:"yellow",
+        borderRadius:40,
+    },
+    scrollView1: {
+
+    },
+
+});
